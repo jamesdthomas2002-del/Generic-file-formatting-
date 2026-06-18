d9b921df53654cfd624832d5ee6e83b194abc5f2
jamesdthomas2002-del <jamesdthomas2002@gmail.com>
2026-06-18 08:19:04 +0100

    Update Strip Characters and create new columns.py

--- Strip Characters and create new columns.py
+++ Strip Characters and create new columns.py
@@ -1,18 +1,18 @@
 import pandas as pd 
 
 FILE_PATH = r"File Path"
-FILTER_COL = 'Add all the cols you need'
-FILTER_COL2 = 'A column i created'
+FILTER_COL = 'Add as many filter columns as you want'
+FILTER_COL2 = 'This was a column i added'
 
 df = pd.read_csv(FILE_PATH, lineterminator = '\n', encoding = 'utf-8-sig')
-df = df.drop(columns=['\r']) # gets rid of any columns that are empty and start with \r, useful if importing csv's
+df = df.drop(columns=['\r']) # I was woring with a csv, it got rid of an 'empty' column filled with \r
 df[FILTER_COL]= df[FILTER_COL].str.strip()
 df[FILTER_COL] = df[FILTER_COL].astype(str)
-df[FILTER_COL2] = df[FILTER_COL].str[-4:] # creates a new column based of the old one
-df[FILTER_COL2] = df[FILTER_COL2].str.strip(')')
-df[FILTER_COL] = df[FILTER_COL].str[:-7]
+df[FILTER_COL2] = df[FILTER_COL].str[-4:-1] #indexed the characters i wanted from the column already in the data set and created a new column populated with the values
+#df[FILTER_COL2] = df[FILTER_COL2].str.strip(')')
+df[FILTER_COL] = df[FILTER_COL].str[:-7] # stripped off the characters i no longer wanted in the original column
 
 print(df.head())
 
 
-df.to_csv(r"File Path ")
+df.to_csv(r"File Path", index=False)
